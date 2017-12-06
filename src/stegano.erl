ba8aaa7c6d632bd3ba1325c40bd43c1dcbf3b09c
Ilya Gusev <phoenixilya@gmail.com>
2017-12-06 21:44:35 +0300

    Web server GET

--- src/stegano.erl
+++ src/stegano.erl
@@ -1,38 +1,30 @@
-%%%-------------------------------------------------------------------
-%%% @author yallen
-%%% @copyright (C) 2017, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 05. Дек. 2017 1:12
-%%%-------------------------------------------------------------------
 -module(stegano).
--author("yallen").
+-author("Ilya Gusev").
 -include_lib("stdlib/include/assert.hrl").
 
-%% API
--export([main/0]).
+-export([cipher/4, decipher/2]).
 
-cipher(InputImgFilename, OutputImgFilename, TextFilename, BitsUsedCount) ->
+cipher(InputImgFilename, OutputImgFilename, Text, BitsUsedCount) ->
   ?assert(BitsUsedCount =< 8 ),
   ?assert(8 rem BitsUsedCount =:= 0),
-  {ok, Text} = file:read_file(TextFilename),
-  [Info, Width, Height, PixelBytes] = read_bmp(InputImgFilename),
+%%  {ok, Text} = file:read_file(TextFilename),
+  [Info, _, _, PixelBytes] = read_bmp(InputImgFilename),
   TextSize = bit_size(Text),
   FlaggedText = <<TextSize:32, Text/bitstring>>,
   NewPixelBytes = insert_bits(BitsUsedCount, FlaggedText, PixelBytes),
-  write_bmp(OutputImgFilename, Info, NewPixelBytes).
+  Bin = write_bmp(OutputImgFilename, Info, NewPixelBytes),
+  Bin.
 
-decipher(ImgFilename, TextFilename, BitsUsedCount) ->
+decipher(ImgFilename, BitsUsedCount) ->
   ?assert(BitsUsedCount =< 8 ),
   ?assert(8 rem BitsUsedCount =:= 0),
-  {ok, File} = file:open(TextFilename, [write]),
+%%  {ok, File} = file:open(TextFilename, [write]),
   [_, _, _, Pixels] = read_bmp(ImgFilename),
   BitsList = get_bits(BitsUsedCount, Pixels),
   Bin = list_to_bits(BitsList, BitsUsedCount),
   <<Size:32, _/binary>> = Bin,
   <<_:32, Text:Size/bitstring, _/binary>> = Bin,
-  file:write(File, Text),
+%%  file:write(File, Text),
   Text.
 
 read_bmp(FileName) ->
@@ -50,7 +42,8 @@ write_bmp(FileName, Info, PixelBytes) ->
   {ok, File} = file:open(FileName,[write]),
   file:write(File, Info),
   Bin = list_to_binary(PixelBytes),
-  file:write(File, Bin).
+  file:write(File, Bin),
+  <<Info/bitstring, Bin/bitstring>>.
 
 insert_bits(BitsUsedCount, Text, PixelBytes) when bit_size(Text) >= BitsUsedCount ->
   <<NewBits:BitsUsedCount, CurrentText/bitstring>> = <<Text/bitstring>>,
@@ -75,6 +68,6 @@ list_to_bits([X | T], Acc, BitsUsedCount) ->
 list_to_bits([], Acc, _) ->
   Acc.
 
-main() ->
-  cipher("lenna.bmp", "lenna_c.bmp", "text.txt", 2),
-  decipher("lenna_c.bmp", "text1.txt", 2).
+%%main() ->
+%%  cipher("lenna.bmp", "lenna_c.bmp", "text.txt", 2),
+%%  decipher("lenna_c.bmp", "text1.txt", 2).
